634e6a7f2b063c1c13779216d382d74f1d641add
Mathew Morse <mrm1779@gmail.com>
2021-12-21 14:43:44 -0500

    Connect to mongo, save to the database, get all sticky notes api

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 const http = require('http');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { response } = require('express');
 
-const hostname = '127.0.0.1';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+
+var conn = mongoose.createConnection('mongodb://localhost:27017/sticky-database', {useUnifiedTopology: true});
+
+const stickyNote = conn.model('StickyNote', new mongoose.Schema({
+  note: String
+}));
+
+async function saveToDatabase(sticky) {
+  let response = await sticky.save();
+
+  return response;
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.post('/stickyNoteAdd', (req, res) => {
+  var stickyMessage = req.body.note;
+  let sticky = new stickyNote({
+    note: stickyMessage
+  });
+
+  saveToDatabase(sticky).then((response) => {
+    res.send(response);
+  });
+});
+
+app.get('/getAllSticky', (req, res) => {
+  stickyNote.find({}).then(function(response) {
+    res.send(response);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 });
