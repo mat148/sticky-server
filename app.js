77054489f6284ef1d5249ba913c16bef3db30f1f
Mathew Morse <mrm1779@gmail.com>
2021-12-27 16:24:48 -0800

    Long overdue commit, lots of stuff

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { response } = require('express');
+const sanitizeHtml = require('sanitize-html');
 
 const app = express();
 const port = 3000;
@@ -12,10 +13,17 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
+const oneDay = 1000 * 60 * 60 * 24;
+const today = JSON.stringify(new Date());
+
 var conn = mongoose.createConnection('mongodb://localhost:27017/sticky-database', {useUnifiedTopology: true});
 
 const stickyNote = conn.model('StickyNote', new mongoose.Schema({
-  note: String
+  note: String,
+  createDate: String,
+  endDate: String,
+  reported: Boolean,
+  fingerPrint: String
 }));
 
 async function saveToDatabase(sticky) {
@@ -24,23 +32,62 @@ async function saveToDatabase(sticky) {
   return response;
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+app.get('/', (req, res) => {});
 
 app.post('/stickyNoteAdd', (req, res) => {
-  var stickyMessage = req.body.note;
+  const currentDate = new Date();
+  var adjustedDate = new Date();
+  adjustedDate.setDate(adjustedDate.getDate() + 14);
+  var stickyMessage = sanitizeHtml(req.body.note, {
+    allowedTags: ['strong', 'i', 'h1', 'h2', 'h3', 'code', 'u' , 's'],
+    allowedAttributes: {}
+  });
+  var fingerPrint = req.body.fingerPrint;
+
+  console.log(fingerPrint);
+
   let sticky = new stickyNote({
-    note: stickyMessage
+    note: stickyMessage,
+    createDate: JSON.stringify(currentDate),
+    endDate: JSON.stringify(adjustedDate),
+    reported: false,
+    fingerPrint: fingerPrint
   });
 
   saveToDatabase(sticky).then((response) => {
+    console.log('Save to Database');
+    res.send(response);
+  });
+});
+
+app.post('/stickyNoteEdit', (req, res) => {
+  var noteID = req.body.noteID;
+  var objectId = mongoose.Types.ObjectId(noteID);
+  var note = req.body.note;
+
+  conn.collections.stickynotes.updateOne({_id:objectId}, {$set:{note:note}}, function(err, result) {
+    if(err) {
+      console.error(err);
+    } else res.send(result);
+  })
+});
+
+app.post('/stickyNoteDelete', (req, res) => {
+  var noteID = req.body.noteID;
+  var objectId = mongoose.Types.ObjectId(noteID);
+
+  conn.collections.stickynotes.deleteOne({_id: objectId})
+  .then((response) => {
     res.send(response);
   });
 });
 
 app.get('/getAllSticky', (req, res) => {
-  stickyNote.find({}).then(function(response) {
+  var filter = {"endDate": {
+    $gt: today
+  }}
+
+  stickyNote.find(filter).then(function(response) {
     res.send(response);
   });
 });
